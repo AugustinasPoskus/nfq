71eef0613de9726cb17546b32ca62dfef9a63dcd
Augustinas Poškus <Augustinas Pokus>
2017-02-12 22:37:09 +0200

    test

--- connect_db.php
+++ connect_db.php
@@ -1,8 +1,8 @@
 <?PHP
-DEFINE ('DB_USER','b2adc5e7d94acb');
-DEFINE ('DB_PASSWORD', '88fce72b');
+DEFINE ('DB_USER','b40e20d30165e7');
+DEFINE ('DB_PASSWORD', 'efe1d178');
 DEFINE ('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net');
-DEFINE ('DB_NAME', 'heroku_eec8414836f0513');
+DEFINE ('DB_NAME', 'heroku_0a0df9fb0b9482b');
 $db_conx = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
 OR die('Could not connect to mySQL ' . mysqli_connect_error());
 ?>
